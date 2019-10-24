5081e8ff2db1bbf7d3b56a1ae2039882681a532b
Minh Duc <minhduc10281996@gmail.com>
2019-10-24 07:35:42 +0700

    Cập nhật bài Layout

--- Layout-No-Bootstrap/js/layout-7.js
+++
@@ -1,9 +0,0 @@
-$(".mobi-button").click(function() {
-    $(".navbar").slideToggle("slow");
-    
-    $(window).resize(function () {
-        if($(window).width() > 576) {
-            $(".navbar").removeAttr('style');
-        }
-    })
-})
